a67d68a91e73a1a9c9d98349344cb81f001479fa
imadouldsaid <imadmadououldsaid2006@gmail.com>
2026-05-21 13:35:47 +0100

    Add files via upload

---
+++ screen/football-screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class FootballPage extends StatefulWidget {
+  const FootballPage({super.key});
+
+  @override
+  State<FootballPage> createState() => _FootballPageState();
+}
+
+class _FootballPageState extends State<FootballPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
