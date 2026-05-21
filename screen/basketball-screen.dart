00d45b84c46c41ede75b420ba0a3d7107c3e78fb
imadouldsaid <imadmadououldsaid2006@gmail.com>
2026-05-21 13:35:28 +0100

    Create basketball-screen.dart

---
+++ screen/basketball-screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class BasketballPage extends StatefulWidget {
+  const BasketballPage({super.key});
+
+  @override
+  State<BasketballPage> createState() => _BasketballPageState();
+}
+
+class _BasketballPageState extends State<BasketballPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
